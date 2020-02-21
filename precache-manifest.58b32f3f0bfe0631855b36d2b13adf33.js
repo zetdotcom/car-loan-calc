2a0924ca4c812bca17c4da8edbf78a11861b92e3
Michal Zet <michal.zadarnowski@gmail.com>
2020-02-21 06:25:20 +0000

    Updates

---
+++ precache-manifest.58b32f3f0bfe0631855b36d2b13adf33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ec984b54166f01080ad0adfa7bbf0a9",
+    "url": "/car-loan-calc/index.html"
+  },
+  {
+    "revision": "2104f287a4ca9016fcb5",
+    "url": "/car-loan-calc/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a502341d48109ef61cd9",
+    "url": "/car-loan-calc/static/js/2.1a244343.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/car-loan-calc/static/js/2.1a244343.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2104f287a4ca9016fcb5",
+    "url": "/car-loan-calc/static/js/main.a7513ed4.chunk.js"
+  },
+  {
+    "revision": "73b1f6c96b5a29c280d0",
+    "url": "/car-loan-calc/static/js/runtime-main.6b4fbf80.js"
+  }
+]);
